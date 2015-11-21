42be876ec312e33131330bcc3d6956dd362782f5
Raphael Audet <raphael.audet@mendix.com>
2015-11-21 22:02:43 +0100

    breaking change refactor : app is now popup based instead of background feature(create) functional feature(update) not working, unknown root cause feature(delete) functional refactor(ajax) adding testing method updated readmed

---
+++ aztab2task.js
@@ -0,0 +1,123 @@
+/*---Ajax functions ---*/
+
+var task = {id: "undefined", owner_id: "undefined", label: "undefined", body: "", url: "" }
+
+function createTask(url) {
+  var xhttp;
+  xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      createdTaskCallback(xhttp); }
+  }
+  var targetUrl=apiUrl + "/tasks";
+  var label=url;
+  var params='label=' + label;
+  xhttp.open("POST", targetUrl , true);
+  xhttp.setRequestHeader('Authorization', 'Token token='+window.localStorage.apiKey);
+  xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  spinnerShow();
+  xhttp.send(params);
+}
+
+function parseJson(xhttp) {
+  response = xhttp.response;
+  // jesponse = JSON.parse(response); //api.azendoo.com format
+  jesponse = JSON.parse(response).data; //app.azendoo.com/api format
+  task.owner_id = jesponse.author;
+  task.id = jesponse.id;
+  task.label = jesponse.label;
+}
+
+function createdTaskCallback(xhttp) {
+  parseJson(xhttp);
+  updateUI();
+  updateTask();
+}
+
+function updatedTaskCallback(xhttp) {
+  parseJson(xhttp);
+  updateUI();
+}
+
+function updateUI() {
+  spinnerHide();
+  debugTask();
+}
+
+function debugTask() {
+  document.getElementById('text').value = task.label;
+  console.log('task_id:'+  task.id);
+  console.log('owner_id:'+  task.owner_id);
+  console.log('label:'+  task.label);
+  console.log('body:'+  task.body);
+  console.log('url:'+  task.body);
+}
+
+function spinnerShow() {
+  document.getElementById('spinner').style.visibility = 'visible';
+}
+
+function spinnerHide() {
+  document.getElementById('spinner').style.visibility = 'hidden';
+}
+
+function updateTask() {
+  var xhttp;
+  xhttp= new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      updatedTaskCallback(xhttp);
+    }
+  }
+  var label = document.getElementById('text').value;
+  var targertUrl=apiUrl + "/tasks/"+task.id;
+  // var params = JSON.stringify({"owner": task.owner_id,"label":label});
+  // var params = JSON.stringify({"owner":task.owner_id, "owner":{"id":task.owner_id},"owner_id":task.owner_id});
+  // var params = JSON.stringify({owner: task.owner_id});
+  // var params = JSON.stringify({"owner":task.owner_id ,"label":label});
+  // var params ='{"owner"="' + task.owner_id +'"}';
+  // var params ='{"owner"="' + task.owner_id +'"}';
+  // var params = JSON.stringify({"body":task.label,"label":label});
+  xhttp.open("PUT", targertUrl , true);
+  xhttp.setRequestHeader('Authorization', 'Token token='+window.localStorage.apiKey);
+  xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  spinnerShow();
+  xhttp.send(params);
+}
+
+/*---buttons functions ---*/
+
+function addUrlInBody() {
+  console.log("addUrlInBody");
+}
+
+function deleteTask() {
+  console.log("deleteTask");
+  var xhttp;
+  xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      spinnerHide();
+      // close popup
+    }
+  }
+  var label = document.getElementById('text').value;
+  var targertUrl=apiUrl + "/tasks/"+task.id;
+
+  xhttp.open("DELETE", targertUrl , true);
+  xhttp.setRequestHeader('Authorization', 'Token token='+window.localStorage.apiKey);
+  xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  // xhttp.setRequestHeader('Content-Type', 'application/javascript');
+  spinnerShow();
+  xhttp.send();
+}
+
+function processTab(tab) {
+  if (tab.url.indexOf("http:") != 0 &&
+      tab.url.indexOf("https:") != 0) {
+    createTask(tab.url);
+  } else {
+    // chrome.tabs.executeScript(null, {file: "content_script.js"});
+  }
+
+}
---
+++ popup.local.js
@@ -0,0 +1,11 @@
+var apiUrl="https://api.azenstaging.com/api";
+// var apiUrl="https://api.azenstaging.com";
+// var apiUrl="https://staging.azendoo.com/api";
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('addUrlInBody').addEventListener("click", addUrlInBody);
+  document.getElementById('deleteTask').addEventListener("click", deleteTask);
+  document.getElementById('saveTask').addEventListener("click", updateTask);
+  window.localStorage.apiKey = 'TESTKEY';
+  createTask('test2');
+});
